ad5f17cced577191605a7f3a96cfadeac8ae3349
Travis Brown <travjbrown98@gmail.com>
2018-11-12 17:46:39 -0500

    Initialize count to zero

--- project2.s
+++ project2.s
@@ -1,18 +1,17 @@
 .data
-	emptyInput:	.asciiz "Input is empty."
-	invalidNUm:	.asciiz "Invalid base-31 number."
+	emptyInput: .asciiz "Input is empty"
+	invalidNum: .asciiz "Invalid base-31 number"
 	user_input:	.space 1000
-	longInput:	.asciiz "Input is too long."
-	
+	longInput:	.asciiz "Input is too long."	
 .text
 main:
 	li $v0, 8
 	la $a0, user_input
-	li $a1, 1000
+	li $a1, 16
 	syscall
 	
-	loop:
-		bgt $t0, 3, exit
+	li $t0, 0 #initialize count to zero	
+
 
 	li $v0, 10
 	syscall
