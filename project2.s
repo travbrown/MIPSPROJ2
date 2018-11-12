ae5f6bee56f0819209714a428026e736b45568fc
Travis Brown <travjbrown98@gmail.com>
2018-11-12 17:28:53 -0500

    Declare variables

--- project2.s
+++ project2.s
@@ -1,12 +1,14 @@
 .data
-	emptyInput:	.asciiz "Input is empty"
-	invalidNUm:	.asciiz "Invalid base-31 number
-	user_input:	.space 16
+	emptyInput:	.asciiz "Input is empty."
+	invalidNUm:	.asciiz "Invalid base-31 number."
+	user_input:	.space 1000
+	longInput:	.asciiz "Input is too long."
+	
 .text
 main:
 	li $v0, 8
 	la $a0, user_input
-	li $a1, 16
+	li $a1, 1000
 	syscall
 	
 	loop:
