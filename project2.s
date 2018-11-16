d87d85dcaa71025b5854ea906d050002f5ec577b
Travis Brown <travjbrown98@gmail.com>
2018-11-16 18:24:25 -0500

    Set label for Too Long error

--- project2.s
+++ project2.s
@@ -1,20 +1,33 @@
 .data
-	emptyInput: .asciiz "Input is empty"
-	invalidNum: .asciiz "Invalid base-31 number"
+	emptyInput:	.asciiz "Input is empty"
+	invalidNum:	.asciiz "Invalid base-31 number"
 	user_input:	.space 1000
 	longInput:	.asciiz "Input is too long."	
 .text
 main:
+	#getting user input
+	
 	li $v0, 8
 	la $a0, user_input
 	li $a1, 1000
 	syscall
-	
-exit:	
-	la $a0,
-	li $v0, 4
-	syscall
 
-	li $v0, 10
-	syscall
+	la $t4, user_input
+	lb $t3, 0($t4)
+	
+#Checking for empty input
+	beq $t3, 10, empty_error
+	beq $t3, 0, empty_error
+	
+		
+exit:
+		la $a0, 
+		li $v0, 1
+		syscall
+
+		li $v0, 10
+		syscall
+#Error Branches
+
+too_long_error:
 
